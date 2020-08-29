eabb4fb9c706ad8a7a4b44101093b3e689caf202
David Mauricio Valoyes Porras <dvaloyesp@unbosque.edu.co>
2020-08-28 21:00:33 -0500

    Organización de capas
    Organización del repositorio en las tres capas que se trabajaran: Presentación, negocio y persistencia.

--- index.php
+++ index.php
@@ -8,11 +8,10 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <!--     Fonts and icons     -->
-    <link rel="stylesheet" type="text/css"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- Material Kit CSS -->
-    <link href="assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
+    <link href="Presentacion/assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
 </head>
 
 <body>
@@ -30,7 +29,7 @@
                     <div class="card-body">
 
                         <br>
-                        <img class="img" width="50%" src="./images/banner.jpg" />
+                        <img class="img" width="50%" src="Presentacion//images/banner.jpg" />
 
                         <div>
                             <br>
@@ -49,7 +48,7 @@
 
     <!-- Footer -->
     <?php
-    include "./components/footer.php";
+    include "Presentacion/components/footer.php";
     ?>
     <!-- Footer -->
 </body>
