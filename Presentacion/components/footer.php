3c73a473633b2343a9bc3628ebe0ef02e85cf115
JSVargasQ <42383191+Sagrav10@users.noreply.github.com>
2020-08-31 08:28:53 -0500

    Update footer.php
    Se cambio la ruta del archivo "logoueb1.png" a un servicio de imagenes web https://i.ibb.co/bKXQLrp/logoueb1.png

--- Presentacion/components/footer.php
+++ Presentacion/components/footer.php
@@ -3,11 +3,12 @@
         <nav class="float-right">
             <ul>
                 <li>
-                <a title="Universidad El Bosque" href="https://www.unbosque.edu.co/"><img class="img" width="18%" height="8%" align="center" src="Presentacion/images/logoueb1.png" /></a>
+                    <a title="Universidad El Bosque" href="https://www.unbosque.edu.co/"><img class="img" width="18%"
+                            height="8%" align="center" src="https://i.ibb.co/bKXQLrp/logoueb1.png" /></a>
                     <p align="center">
                         Universidad El Bosque <br>
                         Instalaciones Bogotá: Av. Cra. 9 No. 131 A - 02 <br>
-                        Línea gratuita: 018000 113033 <br> 
+                        Línea gratuita: 018000 113033 <br>
                         PBX: (571) 648 9000 <br>
                     </p>
                 </li>
