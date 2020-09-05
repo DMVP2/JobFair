8b7730d88fd574bd1fcaff9b10b0bc19dbe43085
DMVP2 <42383248+DMVP2@users.noreply.github.com>
2020-09-04 23:41:24 -0500

    Update rutas.php

--- rutas.php
+++ rutas.php
@@ -1,6 +1,6 @@
 <?php
 
-define('CARPETA_RAIZ', 'Softlutions/Softlutions');
+define('CARPETA_RAIZ', 'Softlutions');
 define('RUTA_ASSETS', '/Presentacion/assets/');
 define('RUTA_IMAGENES', '/Presentacion/images/');
 define('RUTA_FOTOS', '/Presentacion/img/');
