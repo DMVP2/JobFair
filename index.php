ecaab30beb8382431d8b3bd630e34c631a20b4b6
JSVargasQ <42383191+Sagrav10@users.noreply.github.com>
2020-09-07 23:01:57 -0500

    Formulario registrar empresa

--- index.php
+++ index.php
@@ -19,6 +19,18 @@ include_once('./rutas.php')
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- Material Kit CSS -->
     <link href="Presentacion/assets/css/material-dashboard.css" rel="stylesheet" />
+
+    <!-- SCRIPTS BOOSTRAP -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
 </head>
 
 <body>
@@ -44,9 +56,10 @@ include_once('./rutas.php')
                             <br>
                             <button type="submit" class="btn btn-primary"
                                 onclick="window.location.href='Presentacion/login.php'">Iniciar Sesión</button>
-                            <button type="submit" class="btn btn-primary"
-                                onclick="window.location.href='Presentacion/registroEstudiante.php'">Crear
+                            <button type="submit" class="btn btn-primary" data-toggle="modal"
+                                data-target="#exampleModal">Crear
                                 Cuenta</button>
+
                             <br>
                             <br>
                             <br>
@@ -66,6 +79,29 @@ include_once('./rutas.php')
     include "Presentacion/components/footer.php";
     ?>
     <!-- Footer -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"><strong>¿Eres Estudiante o eres una Empresa?</strong>
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body text-center">
+                    <button type="button" class="btn btn-outline-primary"
+                        onclick="window.location.href='Presentacion/registroEstudiante.php'">ESTUDIANTE</button>
+                    <button type="button" class="btn btn-outline-primary"
+                        onclick="window.location.href='Presentacion/registroEmpresa.php'">EMPRESA</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal -->
 </body>
 
 </html>
